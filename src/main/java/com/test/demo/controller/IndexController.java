d55eb03fb9bcd4d48a6f388c372bc7088cc5ce6e
Administrator <1>
2018-05-18 17:51:50 +0800

    1

---
+++ src/main/java/com/test/demo/controller/IndexController.java
@@ -0,0 +1,27 @@
+package com.test.demo.controller;
+
+import com.test.demo.util.SignUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public  String receiverMsg(@RequestParam("signature") String signature,
+                               @RequestParam("timestamp") String timestamp,
+                               @RequestParam("nonce") String nonce,
+                               @RequestParam("echostr") String echostr){
+        // 微信加密签名
+        // 时间戳
+        // 随机数
+        // 随机字符串
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+            return echostr;
+        }
+        return "fail";
+    }
+
+}
